a1d8f4391aa99fb320f84c146ebf957db5cc3fac
cagataycali <cagataycali@icloud.com>
2018-11-14 15:31:01 +0300

    🎉  Init

---
+++ main.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy import fftpack
+import matplotlib.pyplot as plt
+
+# read local image
+img = plt.imread('./elephant.png')
+
+# First a 1-D  Gaussian
+t = np.linspace(-10, 10, 30)
+bump = np.exp(-0.1*t**2)
+bump /= np.trapz(bump) # normalize the integral to 1
+
+# make a 2-D kernel out of it
+kernel = bump[:, np.newaxis] * bump[np.newaxis, :]
+
+# Padded fourier transform, with the same shape as the image
+# We use :func:`scipy.signal.fftpack.fft2` to have a 2D FFT
+kernel_ft = fftpack.fft2(kernel, shape=img.shape[:2], axes=(0, 1))
+
+# convolve
+img_ft = fftpack.fft2(img, axes=(0, 1))
+# the 'newaxis' is to match to color direction
+img2_ft = kernel_ft[:, :, np.newaxis] * img_ft
+img2 = fftpack.ifft2(img2_ft, axes=(0, 1)).real
+
+# clip values to range
+img2 = np.clip(img2, 0, 1)
+
+# plot output
+plt.figure()
+plt.imshow(img2)
+plt.savefig('output.png')
